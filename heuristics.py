dad25e3667f65b59e7da0b6e0b4988e37f926341
Ahmed Waseem <ahmedwaseem2001@gmail.com>
2026-02-24 09:00:00 +0500

    Add heuristics

---
+++ heuristics.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+import math
+
+def manhattan(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def euclidean(a, b):
+=======
+import math
+
+def manhattan(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def euclidean(a, b):
+>>>>>>> 45ecbcd2f9039abf6606246d86980f912bf29977
+    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
